27f7b3d5afd5b87b438b7b504ee99a5160acaf15
jgvasta <julia.vasta@gmail.com>
2025-08-19 23:14:37 -0400

    Delete further unused base tiles

--- script.js
+++ script.js
@@ -15,16 +15,6 @@ const keyMap = {
   "Add your name if you'd like credit (optional)": "submitterName"
 };
 
-/*
-// Add Stadia_StamenTerrain tiles
-L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 18,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
-}).addTo(map);
-*/
-
 // Add ArcGIS world topo map tiles
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
